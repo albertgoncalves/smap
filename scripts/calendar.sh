80b5b82927e71846556f78cf0767179821bf0f04
albertgoncalves <goncalves.albert@gmail.com>
2021-02-13 22:25:19 -0500

    simplify calendar queries

--- scripts/calendar.sh
+++ scripts/calendar.sh
@@ -2,9 +2,8 @@
 
 set -eux
 
-team_id=$1
-date_start=$2
-date_end=$3
+date_start=$1
+date_end=$2
 
-curl "https://statsapi.web.nhl.com/api/v1/schedule?teamId=${team_id}&startDate=${date_start}&endDate=${date_end}" \
-    > "$WD/out/calendar_${team_id}_${date_start}_${date_end}.json"
+curl "https://statsapi.web.nhl.com/api/v1/schedule?startDate=${date_start}&endDate=${date_end}" \
+    > "$WD/out/calendar_${date_start}_${date_end}.json"
